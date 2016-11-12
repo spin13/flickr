bf8909d5c71bcc683b6f4911ddd0aaf684639330
spin13 <spin.eco.info@gmail.com>
2016-11-12 16:13:31 +0900

    first

--- flickr_api.py
+++ flickr_api.py
@@ -1,16 +1,17 @@
-
 import env
 import json
 import requests
+import random
+from time import sleep
 
 
 def to_url(json_data):
     return 'https://farm%s.staticflickr.com/%s/%s_%s.jpg' % (i['farm'], i['server'], i['id'], i['secret'])
 
 
-def get_picts(word, N):
+def get_picts(word='', N=1, sort_algo=''):
     opts = {
-        'sort': 'relevance',
+        'sort': sort_algo,
         'privacy_filter': 1,
         'content_type': 1,
         'per_page': N,
@@ -24,9 +25,26 @@ def get_picts(word, N):
     res = requests.get(url, params=opts)
     return res.json()['photos']['photo']
 
-if __name__ == '__main__':
-    a = get_picts('cat', 3)
-    for i in a:
-        print(to_url(i))
 
+def random_url():
+    ''' 
+    sort:
+        
+        date-posted-desc    アップロード日時の新しい順
+        date-posted-asc アップロード日時の古い順
+        date-taken-asc  撮影日時の古い順
+        date-taken-desc 撮影日時の新しい順
+        interestingness-desc    人気の高い順
+        interestingness-asc 人気の低い順
+        relevance   関連度の高い順
+    '''
+    words = [
+        'cat', 'cat cute', 'kitten', 'kitten cute'
+    ]
+    algos = [
+        'date-posted-desc', 'date-taken-desc', 'interestingness-desc', 'relevance'
+    ]
+
+    res = get_picts(word=random.choice(words), N=100, sort_algo=random.choice(algos))
+    return to_url(random.choice(res))
 
